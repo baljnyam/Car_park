94a6c27eb0864608b7d5841148cb48693af53cb7
baljnyam <baljnyam@gmail.com>
2017-08-30 08:39:39 +1000

    Update PaystationController.java
    Program spec added

--- src/bcccp/carpark/paystation/PaystationController.java
+++ src/bcccp/carpark/paystation/PaystationController.java
@@ -16,6 +16,8 @@ public class PaystationController
 
 	public PaystationController(ICarpark carpark, IPaystationUI ui) {
 		//TODO Implement constructor
+		this.carpark = carpark;
+		this.ui = ui;
 	}
 
 
@@ -23,6 +25,19 @@ public class PaystationController
 	@Override
 	public void ticketInserted(String barcode) {
 		// TODO Auto-generated method stub
+		/*
+		if the controller state is IDLE 
+			request the carpark to return the adhoc ticket identified by the barcode
+			if a ticket is returned and is current and not paid 
+				request the carpark to calculate the charge 
+				display the charge 
+				transition controller to WAITING state 
+			otherwise
+				make the UI 'beep'
+				transition controller to REJECTED state
+		otherwise
+			make the UI 'beep'
+		*/
 		
 	}
 
@@ -31,7 +46,14 @@ public class PaystationController
 	@Override
 	public void ticketPaid() {
 		// TODO Auto-generated method stub
-		
+		/*
+		if the controller is in the WAITING state 
+			record the payment time and charge for the adhoc ticket 
+			print payment details on ticket 
+			transition controller to PAID state 
+		otherwise
+			make the UI ‘beep’ 
+		*/
 	}
 
 
@@ -39,7 +61,12 @@ public class PaystationController
 	@Override
 	public void ticketTaken() {
 		// TODO Auto-generated method stub
-		
+		/*
+		if the controller is in the WAITING, PAID, or REJECTED states 
+			transition the controller to the IDLE state 
+		otherwise
+			make the UI 'beep'
+		*/
 	}
 
 	
